929917638dad2b48d39be2c5c14abf53b97c5cbe
Damaximum <79727388+Damaximum@users.noreply.github.com>
2021-08-18 18:35:19 -0700

    added models, and typeDefs. Some resolver stuff added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,6 +14,32 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
   },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  };
 };
 
 module.exports = resolvers;
